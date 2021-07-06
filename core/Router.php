d59d1a3ea89d521b58976985ee548bb8d4c5b956
yotalha <talha.coder9@gmail.com>
2021-07-06 20:04:18 +0500

    status code 404 added

--- core/Router.php
+++ core/Router.php
@@ -5,15 +5,18 @@ namespace app\core;
 class Router
 {
     public Request $request;
+    public Response $response;
     protected array $routes = [];
 
     /**
      * Router constructor.
-     * @param Request $request
+     * @param \app\core\Request $request
+     * @param \app\core\Response $response
      */
-    public function __construct(Request $request)
+    public function __construct(Request $request, Response $response)
     {
         $this->request = $request;
+        $this->response = $response;
     }
 
     public function get($path, $callback){
@@ -27,6 +30,7 @@ class Router
         $method = $this->request->getMethod();
         $callback = $this->routes[$method][$path] ?? false;
         if($callback === false){
+            $this->response->setStatusCode(404);
             return "Not Found";
         }
         if(is_string($callback)){
