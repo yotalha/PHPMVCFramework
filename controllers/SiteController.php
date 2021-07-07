360960f68cdb3446058b67a5479e8b086f07a41e
yotalha <talha.coder9@gmail.com>
2021-07-07 15:06:18 +0500

    part1 phpmvcframework routes,controllers, views completed

---
+++ controllers/SiteController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace app\controllers;
+
+use app\core\Application;
+use app\core\Controller;
+use app\core\Request;
+
+/**
+ * Class SiteController
+ *
+ * @package app\controllers
+ */
+
+class SiteController extends Controller
+{
+    public function home()
+    {
+        $params = [
+            'name' => "Talha"
+        ];
+        return $this->render('home', $params);
+    }
+
+    public function contact()
+    {
+        return $this->render('contact');
+    }
+
+    public function handleContact(Request $request)
+    {
+        $body = $request->getBody();
+        echo '<pre>';
+        var_dump($body);
+        echo '</pre>';
+        exit;
+        return 'Handling submitted data';
+    }
+}
---
+++ views/register.php
@@ -0,0 +1,35 @@
+<h1>Register</h1>
+
+<form action="" method="post">
+    <div class="row">
+        <div class="col">
+            <div class="form-group">
+                <label>FirstName</label>
+                <input type="text" name="firstname" class="form-control" >
+            </div>
+        </div>
+        <div class="col">
+            <div class="form-group">
+                <label>lastName</label>
+                <input type="text" name="lastname" class="form-control" >
+            </div>
+        </div>
+    </div>
+    <div class="form-group">
+        <label>Email</label>
+        <input type="text" name="email" class="form-control" >
+    </div>
+    <div class="form-group">
+        <label>Password</label>
+        <input type="password" name="password" class="form-control" >
+    </div>
+    <div class="form-group">
+        <label>Password</label>
+        <input type="password" name="password" class="form-control" >
+    </div>
+    <div class="form-group">
+        <label>Confirm Password</label>
+        <input type="password" name="confirmPassword" class="form-control" >
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
